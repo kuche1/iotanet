1fa218a3a4294e95ce1ab44ef19367ab8e11af53
kuche1 <znamsiparolata4esno@abv.bg>
2024-09-17 00:24:58 +0300

    update

--- epsilon.py
+++ epsilon.py
@@ -1,11 +1,12 @@
 
 import socket
 import argparse
-from typing import Iterator
-
+from typing import Iterator, Callable, Any
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicKey, RSAPrivateKey
+import threading
+import time
 
 NOT_FOR_YOU = '0'.encode()
 YES_FOR_YOU = '1'.encode()
@@ -16,6 +17,15 @@ Ip = str
 Port = int
 Addr = tuple[Ip,Port]
 Node = tuple[Addr,Public_key]
+Socket = socket.socket
+
+class Bucket:
+    def __init__(s, data:Any) -> None:
+        s.set(data)
+    def set(s, data:Any) -> None:
+        s.data = data
+    def get(s) -> Any:
+        return s.data
 
 def cut_until(msg:bytes, until:bytes) -> tuple[bytes, bytes]:
     idx = msg.index(until)
@@ -51,9 +61,9 @@ def decrypt(msg:bytes, key:Private_key) -> bytes:
         )
     )
 
-# TODO ne mi haresva tova kak se poluchava
-# ideally, tova e otdelen thread i drugite procesi prosto si govorqt s nego
-def handle_incoming(port:int, private_key:Private_key) -> Iterator[bytes]:
+# perhaps it's best if instead of a callback all the messages
+# get written to a folder on the FS
+def handle_incoming(buck_sock:Bucket, port:int, private_key:Private_key, on_recv:Callable[[bytes],None]) -> None:
 
     sock = socket.socket()
 
@@ -61,9 +71,17 @@ def handle_incoming(port:int, private_key:Private_key) -> Iterator[bytes]:
 
     sock.listen()
 
+    buck_sock.set(sock)
+
+    # print('handler listening')
+
     while True:
 
-        client_sock, _client_addr = sock.accept()
+        try:
+            client_sock, _client_addr = sock.accept()
+        except OSError:
+            # thread owner has closed the socket
+            break
 
         # TODO everything from this point on should be in a new thread
 
@@ -82,15 +100,25 @@ def handle_incoming(port:int, private_key:Private_key) -> Iterator[bytes]:
         
         succ, actual_data = handle_msg(payload, private_key)
         if succ:
-            yield actual_data
+            on_recv(actual_data)
 
     sock.close()
 
 def send_directly(payload:bytes, to:Addr) -> None:
     # TODO we should do the "wait for some time before sending then send all in mixed order" trick
+
+    print(f'request to send something to `{to}`')
+
     sock = socket.socket()
-    sock.connect(to)
+
+    try:
+        sock.connect(to)
+    except ConnectionRefusedError:
+        print(f'connection refused to `{to}`')
+        return
+
     sock.sendall(payload)
+
     sock.close()
 
 def send(payload:bytes, path:list[Node]) -> None:
@@ -101,19 +129,29 @@ def send(payload:bytes, path:list[Node]) -> None:
 
     for [ip, port], public_key in reversed(path):
 
+        # print(f'payload to send {payload!r}')
+
         if is_receiver:
             is_receiver = False
             payload = YES_FOR_YOU + payload
         else:
             payload = NOT_FOR_YOU + ip.encode() + ':'.encode() + str(port).encode() + ':'.encode() + payload
 
+        # print(f'with prefix {payload!r}')
+
         payload = encrypt(payload, public_key)
+
+        # print(f'encrypted {payload!r}')
 
     send_directly(payload, (ip, port))
 
 def handle_msg(payload:bytes, private_key:Private_key) -> tuple[bool, bytes]:
 
+    # print(f'got {payload!r}')
+
     payload = decrypt(payload, private_key)
+
+    # print(f'decrypted {payload!r}')
 
     if payload.startswith(NOT_FOR_YOU):
 
@@ -133,7 +171,7 @@ def handle_msg(payload:bytes, private_key:Private_key) -> tuple[bool, bytes]:
 
         payload = payload[len(YES_FOR_YOU):]
 
-        payload = decrypt(payload, private_key)
+        # print(f'without the prefix {payload!r}')
 
         return True, payload
 
@@ -141,20 +179,50 @@ def handle_msg(payload:bytes, private_key:Private_key) -> tuple[bool, bytes]:
 
         assert False
 
-def main() -> None:
+def test() -> None:
 
-    print('dicks')
+    ####
+    #### enc/dec
+    ####
 
-    # handle_requests(6969)
+    msg = 'fxewagv4reytgesrfdgvfy5ey645r'
+    msg_as_bytes = msg.encode()
+    priv, pub = generate_keys()
+    assert decrypt(encrypt(msg_as_bytes, pub), priv).decode() == msg
 
-    private_key, public_key = generate_keys()
+    ####
+    #### send/recv self
+    ####
 
-    data = encrypt(b'hui 123', public_key)
+    priv, pub = generate_keys()
 
-    print(data)
+    buck_sock = Bucket(None)
+    on_recv = lambda data: print(f'received: {data!r}')
+    thr = threading.Thread(target=handle_incoming, args=(buck_sock, 6969, priv, on_recv))
+    thr.start()
+
+    print('waiting')
+
+    while buck_sock.get() != None:
+        time.sleep(0.1)
+    
+    print('ready')
+
+    msg = 'sex sex SEEEXXXX $###33333XXXXXXHHHHHHs'
+
+    msg_as_bytes = msg.encode()
+
+    path = [
+        (('127.0.0.1', 6969), pub),
+    ]
+
+    send(msg_as_bytes, path)
+
+    buck_sock.get().close() # a bit forced
+
+    thr.join()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser('Epsilon Node')
+    parser = argparse.ArgumentParser('Epsilon Node; call directly to run tests')
     args = parser.parse_args()
-
-    main()
+    test()
